e4f02aa23f2ebadf263fa28b9e4cabd25d870c25
Saravana-2816 <221501123@rajalakshmi.edu.in>
2025-05-14 22:39:26 +0530

    palindrome

--- Problems/Palindrome.java
+++ Problems/Palindrome.java
@@ -3,11 +3,15 @@ public class Palindrome {
     public static void main(String[] args) {
         Scanner ob = new Scanner(System.in);
         String str=ob.nextLine();
-        String res="";
-        for(int i=str.length()-1;i>=0;i--){
-            res+=str.charAt(i);
+        int i=0;
+        boolean flag=true;
+        for(int j=str.length()-1;j>=0;i--){
+            if(j>i) break;
+            else {
+                if(str.chartAt(i)!=str.charAt(j)) flag=false;
+            }
         }
-        if(res.equals(str)) System.out.println("palindrome");
-        else System.out.print("Not palindrome");
+        if (flag) System.out.println("palindrome");
+        else System.out.print("not palindrome");
     }
 }
